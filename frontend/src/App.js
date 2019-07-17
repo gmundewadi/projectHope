9b598590d5b17ff3bf0744f40f0fc5170ce89ae6
Gautam Mundewadi <gautammundewadi@cs.ucsb.edu>
2019-07-16 19:59:20 -0700

    basic frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,32 @@ import React from "react";
 
 export default class FetchRSSFeed extends React.Component {
   state = {
-    loading: true
+    loading: true,
+    data: []
   };
 
   async componentDidMount() {
     const url = "http://localhost:8080/articles/";
     const response = await fetch(url);
     const data = await response.json();
+    this.setState({ loading: false });
+    this.setState({ data: data });
     console.log(data);
   }
 
   render() {
     return (
       <div>
-        {this.state.loading ? <div> loading... </div> : <div>person...</div>}
+        {this.state.data.map((dynamicData, key) => (
+          <div>
+            <p>
+              <b>TITLE: {dynamicData.title}</b>
+            </p>
+            <p>PUBDATE: {dynamicData.pubDate}</p>
+            <p>URL: {dynamicData.uri}</p>
+            <p>DESCRIPTION: {dynamicData.description}</p>
+          </div>
+        ))}
       </div>
     );
   }
