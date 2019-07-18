5e0700b92ed8c799cce62a3f3e3014c1c1936edc
Gautam Mundewadi <gautammundewadi@cs.ucsb.edu>
2019-07-18 16:46:11 -0700

    image reader issues

--- RSS_Reader/src/main/java/projectHope/RSS_Reader/RSSReader.java
+++ RSS_Reader/src/main/java/projectHope/RSS_Reader/RSSReader.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 import org.bson.BsonDocument;
@@ -38,14 +42,13 @@ public class RSSReader {
     	mongoClient = new MongoClient(uriVal);
 		updateDB();
 		System.out.println(countArticles());
+		
 	}
+	
 	
 	public static void updateDB() {
         try { 
-
-        	
         	MongoDatabase database = mongoClient.getDatabase("RSS_Reader");
-        	
     		MongoCollection<Document> collection = database.getCollection("Article");
         	FeedFetcher fetcher = new HttpURLFeedFetcher();
         	SyndFeed feed;
@@ -53,14 +56,14 @@ public class RSSReader {
         	Scanner sc = new Scanner(file);
         	while(sc.hasNextLine()) {
         		String url = sc.nextLine();
-    			feed = fetcher.retrieveFeed(new URL(url));
-    			for(Object o: feed.getEntries()) {
+        		feed = fetcher.retrieveFeed(new URL(url));	
+    			for(Object o: feed.getEntries()) {   		
     				SyndEntry entry = (SyndEntry) o;
     				String title = entry.getTitle();
     				String link = entry.getLink();
     				String description = entry.getDescription().getValue();
     				Date pubDate = entry.getPublishedDate();
-    				String uri = entry.getUri();
+    				String uri = entry.getUri();	
     				Document document = new Document();
     				document.append("link", link);
     				long count = collection.countDocuments(new BsonDocument("link", new BsonString(link)));    					
@@ -69,7 +72,6 @@ public class RSSReader {
     				}
     				document.append("title", title);
     				document.append("description", description);
-    			
     				document.append("pubDate", pubDate);
     				document.append("uri", uri);
     		        collection.insertOne((document));
@@ -79,7 +81,7 @@ public class RSSReader {
         } catch (IllegalArgumentException e) {
 			System.out.println("Illegal Arguement Exception");
 			e.printStackTrace();
-		} catch (MalformedURLException e) {
+        } catch (MalformedURLException e) {
 			System.out.println("MalformedURL Exception");
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -91,9 +93,46 @@ public class RSSReader {
 		} catch (FetcherException e) {
 			System.out.println("Fetchter Exception");
 			e.printStackTrace();
-		}	
+		}
+	}
+		
+ 
+	// BUGS present
+	private static List<String> getImageLinks(String url) {
+		try {
+			ArrayList<String> result = new ArrayList<>();
+			URL rssURL = new URL(url);
+			BufferedReader in = new BufferedReader(new InputStreamReader(rssURL.openStream()));
+			String line;
+			while((line = in.readLine()) != null) {
+				if(line.contains(("<media:content"))) {
+					int firstPos = line.indexOf("<media:content");
+					String temp = line.substring((firstPos));
+					temp = temp.replace("<media:content medium='image' url =", "");
+					int lastPos = temp.indexOf("/><media:content");
+					temp = temp.substring(0, lastPos);
+					temp = temp.replace("<media:content", "");
+					temp = temp.replace("medium=\"image\"", "");
+					temp = temp.replace("  url=\"","");
+					temp = temp.replace("\" height=\"619\" width=\"1100\" ","");
+					result.add(temp);
+				}
+			}
+			in.close();
+			return result;
+			
+		} catch (MalformedURLException e) {
+			System.out.println("MalformedURL Exception");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IO Exception");
+			e.printStackTrace();
+		}
+		// unreachable return statement. Only to compile.
+		return null;
 	}
 	
+
 	public static int countArticles() {
 		MongoDatabase database = mongoClient.getDatabase("RSS_Reader");
 		MongoCollection<Document> collection = database.getCollection("Article");
